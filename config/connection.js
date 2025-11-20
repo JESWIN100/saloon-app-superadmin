b46bba488bdbca6221bc174432c58d9abd7bd745
JESWIN100 <josepjjeswin20@gmail.com>
2025-11-20 17:09:32 +0530

    chnaged the connection info

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,22 @@
 const mysql = require('mysql');
-const connection = mysql.createConnection({
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
   host: 'sg2plzcpnl506716.prod.sin2.secureserver.net',
   user: 'gulfdigit',
   password: 'Telepathy321#',
-  database: 'gulfdigit'
+  database: 'gulfdigit',
+
+  connectTimeout: 20000,
+  acquireTimeout: 20000,
+  waitForConnections: true,
+  queueLimit: 0
 });
 
-module.exports = connection;
+console.log('Database connected!');
+// Prevent crash on dropped connections
+pool.on("error", (err) => {
+  console.error("MySQL Pool Error:", err.code);
+});
+
+module.exports = pool;
