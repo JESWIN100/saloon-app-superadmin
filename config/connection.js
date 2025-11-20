76373b0c71c3f1fa10f6b4e741afa15394b3ec96
JESWIN100 <josepjjeswin20@gmail.com>
2025-11-20 14:24:04 +0530

    changed the connection info

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'db1'
+  host: 'sg2plzcpnl506716.prod.sin2.secureserver.net',
+  user: 'gulfdigit',
+  password: 'Telepathy321#',
+  database: 'gulfdigit'
 });
 
 module.exports = connection;
